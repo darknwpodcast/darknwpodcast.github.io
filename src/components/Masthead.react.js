e137784c979c3bf14096b792922a651951d9488c
Kairsten Fay <kairsten.fay@gmail.com>
2025-08-02 00:48:26 -0700

    Modify fonts

--- src/components/Masthead.react.js
+++ src/components/Masthead.react.js
@@ -6,7 +6,7 @@ function Masthead(props) {
       <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
         <div className="d-flex justify-content-center">
           <div className="text-center">
-            <h1 className="mx-auto my-0 text-uppercase">Dark Northwest Podcast</h1>
+            <h1 className="mx-auto my-0">Dark Northwest Podcast</h1>
             <h2 className="text-white-50 mx-auto mt-2 mb-5">
               Season 1 premieres this fall.
             </h2>
