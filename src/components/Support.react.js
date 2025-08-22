0e9d7fb9c6a51b3a5806bba9b66108f5d1571309
Kairsten Fay <kairsten@meta.com>
2025-08-21 20:49:36 -0700

    Add ko-fi

--- src/components/Support.react.js
+++ src/components/Support.react.js
@@ -3,6 +3,7 @@ function platforms(props) {
   if (props?.platforms === undefined) {
     platforms = {
       patreon: "https://www.patreon.com/DarkNWPodcast",
+      kofi: "https://ko-fi.com/darknwpodcast",
     };
   } else {
     platforms = props.platforms;
@@ -15,6 +16,11 @@ function platforms(props) {
         {platforms["patreon"] ? (
           <a className="mx-2" href={platforms["patreon"]}>
             <i className="fa-brands fa-patreon"></i>
+          </a>
+        ) : null}
+        {platforms["kofi"] ? (
+          <a className="mx-2" href={platforms["kofi"]}>
+            <i class="fa-solid fa-mug-saucer"></i>
           </a>
         ) : null}
       </div>
