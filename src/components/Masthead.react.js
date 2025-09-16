ac7b57a858a934f3cf9cb9f4697f13aac8a9a65e
Kairsten Fay <kairsten@meta.com>
2025-09-15 23:27:41 -0700

    Adjust title styles

--- src/components/Masthead.react.js
+++ src/components/Masthead.react.js
@@ -9,7 +9,7 @@ function Masthead(props) {
       <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
         <div className="d-flex justify-content-center">
           <div className="text-center">
-            <h1 className="mx-auto my-0">Dark Northwest Podcast</h1>
+            <h1 className="mx-auto my-0"><div className='wet-paint'>Dark Northwest</div>{' '}Podcast</h1>
             <h2 className="text-white-50 mx-auto mt-2 mb-5">
               Season 1 now streaming on all podcast platform providers.
             </h2>
