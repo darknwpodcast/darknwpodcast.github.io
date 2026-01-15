998c9e1970f0096e48dda48d93b7dd2292959e7d
Kairsten Fay <kairsten@meta.com>
2026-01-14 16:37:44 -0800

    Update map pin color to match About section

--- src/components/PNWMap.react.js
+++ src/components/PNWMap.react.js
@@ -31,7 +31,7 @@ function PNWMap() {
 
       const darkIcon = L.divIcon({
         className: "pnw-map-marker",
-        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ba2201" width="32" height="32">
+        html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#9b59b6" width="32" height="32">
           <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
         </svg>`,
         iconSize: [32, 32],
